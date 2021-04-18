93f0ada3dcc4f433dcf52f70745a98906ce021e5
guozelong <gzl407473490@163.com>
2021-04-18 16:02:13 +0800

    feat: finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  title: 'Mooc-UI',
   base: '/moocUI/',
   themeConfig: {
+    nav: [{
+      text: 'Home', link: '/'
+    },{
+      text: 'Github', link: 'https://github.com/test-921/moocUI'
+    },{
+      text: 'Npm', link: 'https://www.npmjs.com/package/mooc-ui2'
+    }],
     sidebar: [
       '/',
       '/componentDocs/card'
